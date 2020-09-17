447dec53afb73ea5ecf50b592ec433711d69d3b1
Jake-Mertz <jakemertz101@gmail.com>
2020-09-17 13:12:11 -0400

    view-products-front-end

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 function ProductListItem(props) {
-  // console.log(props);
   return (
     <div className="card" style={{ width: '18rem' }}>
       <img src={props.image} className="card-img-top" alt="sales product"></img>
