1b45a30f0ba57881cac78642b9f4629fa75ab223
Jake-Mertz <jakemertz101@gmail.com>
2020-09-29 16:16:22 -0400

    small fix

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -38,7 +38,7 @@ class App extends React.Component {
 
   addToCart(product) {
     // const productItem = { product: '' };
-    fetch('api/cart', {
+    fetch('/api/cart', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(product)
