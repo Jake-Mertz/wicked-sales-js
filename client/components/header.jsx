b8f7324097ef76653e5f7fc9f42b0ecf6bf08660
Jake-Mertz <jakemertz101@gmail.com>
2020-09-30 22:48:48 -0400

    backToCatalog added to app logo

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -8,11 +8,12 @@ function Header(props) {
       return ' Items';
     }
   }
+  const backToCatalog = () => props.setView('catalog');
   const goToCart = () => props.setView('cart', {});
   return (
     <div className="d-flex bg-dark text-white">
       <i className="fas fa-dollar-sign fa-lg icon"></i>
-      <h3>Wicked Sales</h3>
+      <h3 onClick={backToCatalog}>Wicked Sales</h3>
       <h4 className="header-cart-text">{props.cartItemCount}{item()}</h4>
       <i className="fas fa-shopping-cart header-cart-icon" onClick={goToCart}></i>
     </div>
