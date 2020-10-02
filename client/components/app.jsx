9df9341f67a53b1a202c46abf4e5b443de6a30b1
Jake-Mertz <jakemertz101@gmail.com>
2020-10-02 17:18:07 -0400

    checkout button media queries

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -10,7 +10,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       view: {
-        name: 'catalog',
+        name: 'cart',
         params: {}
       },
       cart: []
