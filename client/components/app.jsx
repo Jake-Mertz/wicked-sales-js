375f28e979b3a6cddf6887664d3e37b780e74212
Jake-Mertz <jakemertz101@gmail.com>
2020-10-15 12:17:33 -0400

    view state changed back to catalog

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -10,7 +10,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       view: {
-        name: 'checkout',
+        name: 'catalog',
         params: {}
       },
       cart: []
