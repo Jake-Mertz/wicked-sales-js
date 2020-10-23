4cb38e67c7df44f35484c96cbd0c07b5e2dde2bb
Jake-Mertz <jakemertz101@gmail.com>
2020-10-23 13:23:30 -0400

    button events, and demo modal finished

--- client/components/catalog-modal.jsx
+++ client/components/catalog-modal.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 function CatalogModal(props) {
   return (
-    <div className="demo-site-modal">
-      <div>This site is for demonstration purposes only. No real purchases can be made.
-        <button onClick={() => props.setView('catalog', {})} className="demo-site-modal-button">Got it!</button>
+    <div className="demo-modal-background">
+      <div className="demo-site-modal">
+        <div>This site is for demonstration purposes only. No real purchases can be made.
+          <button onClick={() => props.setView('catalog', {})} className="demo-site-modal-button">Got it!</button>
+        </div>
       </div>
     </div>
   );
